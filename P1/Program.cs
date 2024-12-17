de465ceba1aee504413102e8b581156f6c583f2b
lujain8 <lujains477@gmail.com>
2024-12-17 18:08:18 +0300

    p

---
+++ P1/Program.cs
@@ -0,0 +1,70 @@
+﻿/*string name = new string("lujain");
+Console.WriteLine(name);*/
+
+/*int number1 = 4;
+int number2 = 6;
+int sum = 4+6;
+Console.WriteLine(sum);*/
+
+/*int [] num = {1,2,3,4,5};
+for(int i = 0; i<num.Length; i++)
+{
+    Console.WriteLine(num[i]);
+}*/
+
+/*using System.Runtime.CompilerServices;
+string Grade = Console.ReadLine();
+
+switch (Grade)
+{
+    case "A" :
+    Console.WriteLine("Excellent");
+    break;
+    
+    case "B" :
+    Console.WriteLine("Very Good");
+    break;
+    
+    case "C" :
+    Console.WriteLine("Good");
+    break;
+    
+    case "D" :
+    Console.WriteLine("Needs Improvement");
+    break;
+    
+    case "F" :
+    Console.WriteLine("Fail");
+    break;
+}*/
+
+/*public class Movie{
+    public string Title{set; get;}
+    public string Director{set; get;}
+    public double Duration{set; get;}
+
+    public Movie(string _Title , string _Director , double _Duration){
+        
+        Title = _Title ;
+        Director = _Director ;
+        Duration = _Duration ;
+    }
+    public void MovieInfo(){
+    Console.WriteLine($"Title :{Title} Director :{Director} Duration :{Duration}");
+    }
+}
+class program {
+    public static void Main(string[] args){
+    Movie Title = new Movie ("L" , "S" , 2.23);
+    Title.MovieInfo();
+
+    Movie Director = new Movie ("N" , "M" , 2.45);
+    Director.MovieInfo();
+
+    Movie Duration = new Movie ("M" , "A" , 3.30);
+    Duration.MovieInfo();
+}
+}*/
+
+//int value = new int (10,100);
+//Console.WriteLine(max value);
